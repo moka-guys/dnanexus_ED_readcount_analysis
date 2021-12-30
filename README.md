9c2e2f39efbf9a6f0c317232128e9cd89ad8d023
aledj2 <aledjones@nhs.net>
2021-12-30 19:38:14 +0000

    address issues in code review and more

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
-# dnanexus_ED_readcount_analysis
-This app calculates the readcount for a batch of samples for CNV calling using ExomeDepth.
+# dnanexus_ED_readcount_analysis_v1.0.0
+Exome depth is run in two stages. Firstly, read counts are calculated, before CNVs are called using the read counts. Read counts are calculated over the entire genome whereas the CNV calling can be performed using a subpanel.
 
 # What does the app do?
+This app runs the read count calculation stage.
 
-The app downloads the BAMs and its indexes from a specified project in DNAnexus using the given pan numbers. The docker image seglh_exomedepth.tgz uses the BAMs to calculate the readcount file. Further details on the docker image can be found in https://github.com/moka-guys/seglh-cnv/tree/main/exomedepth
+Using the provided DNANexus project and the list of Pan numbers the app downloads BAMs and BAI.
 
-The readcount file is then used as an input for the ED_cnv_calling app https://github.com/moka-guys/dnanexus_ED_cnv_calling
+A Docker image containing Exome depth is downloaded from 001 - In this release [#1220d31](https://github.com/moka-guys/seglh-cnv/commit/1220d31e2eed1d4488eb461e70615a0fad8b5eb1#diff-3325e16df2216827f90fb6abf51b58e8dbc54d67e5bda4bb2f880b38c5ba48f3) is used. #TODO make this a release
 
+The `readCount.R` script is then called, producing a readcount file (`PanXXXXexomedepth_readCount.RData`) which can be used as an input for the ED_cnv_calling app https://github.com/moka-guys/dnanexus_ED_cnv_calling
+
+If the panel of normals is not provided then intrabatch normalisation is performed.
 # Inputs
-DNAnexus project name where the BAMs and indexes are saved in a folder called 'output'
-Reference_genome in build 37
-List of comma seperated pan numbers
-Bedfile covering the capture region
-Optional: panel of normals
+* DNAnexus project name where the BAMs and indexes are saved in a folder called '/output'
+* Reference_genome (*.fa.gz or *.fa) in build 37
+* List of comma seperated pan numbers
+* Bedfile covering the capture region
+* Optional: panel of normals
 
 # Output
 readCount.RData - Read count data and selected references per sample
 readCount.csv - Model parameters and QC metrics output (can be used to build a QC classifier, see below)
+
+# Panel of Normals 
+Run following command to create panel of normals
+`docker run -v /home/dnanexus:/home/dnanexus ${DOCKERIMAGENAME} readCount.R /home/dnanexus/out/exomedepth_output/exomedepth_output/$bedfile_prefix/normals.RData $reference_genome_path $bedfile_path $bam_list`
 
 # Created by
 This app was created within the Viapath Genome Informatics section
--- src/code.sh
+++ src/code.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # exomedepth_cnv_analysis_v1.0.0
 
-# Locally stored Docker image to use
-DOCKERIMAGEFILE=/home/dnanexus/seglh_exomedepth.tgz
-
 # The following line causes bash to exit at any point if there is any error
 # and to output each line as it is executed -- useful for debugging
 set -e -x -o pipefail
@@ -15,36 +12,49 @@ run=${project_name##*_}
 #read the DNA Nexus api key as a variable
 API_KEY=$(dx cat project-FQqXfYQ0Z0gqx7XG9Z2b4K43:mokaguys_nexus_auth_key)
 
-#make output dir
-mkdir -p /home/dnanexus/out/exomedepth_output/exomedepth_output/$bedfile_prefix/
-# make folder to hold downloaded files
-mkdir to_test
+# make output dir and folder to hold downloaded files
+mkdir -p /home/dnanexus/out/exomedepth_output/exomedepth_output/$bedfile_prefix/ /home/dnanexus/to_test
 
-#
-# Download inputs
+mark-section "Downloading inputs"
 # download all inputs
 dx-download-all-inputs --parallel
-#
-#dx download "$bedfile"
 
-# make and cd to test dir
-cd to_test
+mark-section "Determining reference genome"
+if  [[ $reference_genome_name == *.tar* ]]
+	then
+		echo "reference is tarball"
+		exit 1
+elif [[ $reference_genome_name == *.gz ]]
+	then 
+		gunzip $reference_genome_path
+		reference_fasta=$(echo $reference_genome_path | sed 's/\.gz//g')
+elif [[ $reference_genome_name == *.fa ]]
+	then
+		reference_fasta=$reference_genome_path
+fi 
 
 mark-section "determine run specific variables"
 #Extract samplename to name output files
 samplename=$(python -c "basename='$bedfile_prefix'; print basename.split('_R1')[0]")
 echo $samplename
-echo "read_depth_bed = " $bedfile
-echo "reference_genome = " $reference_genome
-echo "panel = " $bamfile_pannumbers
-echo "bedfile_prefix = " $bedfile_prefix
-echo "normals_RData = " $normal_RData
+echo "read_depth_bed="$bedfile
+echo "reference_genome="$reference_fasta
+echo "panel="$bamfile_pannumbers
+echo "bedfile_prefix="$bedfile_prefix
+echo "normals_RData="$normal_RData
+output_RData_file="/home/dnanexus/out/exomedepth_output/exomedepth_output/${bedfile_prefix}/${bedfile_prefix}_readCount.RData"
+echo "output RData file="$output_RData_file
+
+
+mark-section "Download all relevant BAMs"
+# make and cd to test dir
+cd to_test
 # $bamfile_pannumbers is a comma seperated list of pannumbers that should be analysed together.
 # split this into an array and loop through to download BAM and BAI files
 IFS=',' read -ra pannum_array <<<  $bamfile_pannumbers
 for panel in ${pannum_array[@]}
 do
-	# check there is at least one bam file with that pan number to download other wise the dx download command will fail
+	# check there is at least one file with that pan number to download otherwise the dx download command will fail
 	if (( $(dx ls $project_name:output/*001.ba* --auth $API_KEY | grep $panel -c) > 0 ));
 	then
 		#download all the BAM and BAI files for this project/pan number
@@ -52,38 +62,42 @@ do
 	fi
 done
 
-#Get list of all BAMs 
-bam_list=""
+# Get list of all BAMs in to_test  and remove any newlines
+# NB (include full filepath to ensure the output are absolute paths (needed for docker run))
 bam_list="$(ls /home/dnanexus/to_test/*bam | tr '\n' ' ')"
-echo "bam list = " $bam_list
 
-
-# count the files. make sure there are at least 3 samples for this pan number, else stop
-filecount="$(ls *001.ba* | grep . -c)"
-if (( $filecount < 6 )); then
+# count the BAM files. make sure there are at least 3 samples for this pan number, else stop
+filecount="$(ls $bam_list | grep . -c)"
+if (( $filecount < 3 )); then
 	echo "LESS THAN THREE BAM FILES FOUND FOR THIS ANALYSIS" 1>&2
 	exit 1
 fi
 
 # cd out of to_test
-cd ..
+cd /home/dnanexus
 
 mark-section "setting up Exomedepth docker image"
-# load/import locally stored docker image
-docker load -i ${DOCKERIMAGEFILE}
-# get full tag of imported image
-DOCKERIMAGENAME=`tar xfO ${DOCKERIMAGEFILE} manifest.json | sed -E 's/.*"RepoTags":\["?([^"]*)"?.*/\1/'`
-echo "Using image:"${DOCKERIMAGENAME}
+# Location of the ExomeDepth docker file
+docker_file=project-ByfFPz00jy1fk6PjpZ95F27J:file-G6kfZYQ0jy1vZ0BF33KZpQjJ
+# download the docker file from 001_Tools...
+dx download $docker_file --auth "${API_KEY}"
+docker load -i '/home/dnanexus/seglh_exomedepth_1220d31.tgz'
 
-mark-section "Run CNV analysis using docker image"
+
+mark-section "Calculate read depths using docker image"
 # docker run - mount the home directory as a share
-# Write log direct into output folder
-# Get read count for all samples
+# call the readCount.R script
+# supply following arguments
+#  	- output_RData_file path
+#  	- reference_fasta_path 
+#  	- bedfile_path 
+#	- bam_list 
+#	- normals_RData_path
+# The log (PanXXXXexomedepth_readCount.csv) written to same location as the $output_RData_file
+
 
 # Run ReadCount script in docker container
-docker run -v /home/dnanexus:/home/dnanexus ${DOCKERIMAGENAME} readCount.R /home/dnanexus/out/exomedepth_output/exomedepth_output/$bedfile_prefix/"$bedfile_prefix"_readCount.RData $reference_genome_path $bedfile_path $bam_list $normals_RData_path
-# Run command below to create panel of normals
-#docker run -v /home/dnanexus:/home/dnanexus ${DOCKERIMAGENAME} readCount.R /home/dnanexus/out/exomedepth_output/exomedepth_output/$bedfile_prefix/normals.RData $reference_genome_path $bedfile_path $bam_list
+docker run -v /home/dnanexus:/home/dnanexus seglh/exomedepth:1220d31 readCount.R $output_RData_file $reference_fasta $bedfile_path $bam_list $normals_RData_path
 
 # Upload results
 dx-upload-all-outputs
